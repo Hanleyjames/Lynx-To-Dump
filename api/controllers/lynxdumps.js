548b94d6b62cb6d72f081a4540d2ed3af89f9d1c
James Hanley <39565690+Hanleyjames@users.noreply.github.com>
2021-05-28 20:38:29 -0700

    Update lynxdumps.js
    remove some console logging

--- api/controllers/lynxdumps.js
+++ api/controllers/lynxdumps.js
@@ -3,13 +3,9 @@ var nodemailer = require('nodemailer');
 
 exports.dump_url = (req, res, next) => {
   let newsurl = req.body.newsurl;
-  console.log(newsurl);
   let email = req.body.email;
-  console.log(typeof email);
-  console.log(email);
   exec(`lynx -dump ${newsurl}`, (err, stdout, stderr) =>{
     if(err){
-      console.log(err);
       res.status(500).json({
         error: {
           message: err
@@ -25,17 +21,13 @@ exports.dump_url = (req, res, next) => {
         }
       });
       transporter.verify(function(error, success) {
-        console.log("Verifying Project");
         if (err) {
-          console.log("Verification Error");
           res.status(500).json({
             error: {
               message: err
             }
           });
         } else {
-          console.log("Verification Complete");
-          console.log(typeof stdout);
           const message = {
             from: process.env.EMAIL_USERNAME,
             to: email,
@@ -44,16 +36,13 @@ exports.dump_url = (req, res, next) => {
           };
 
           transporter.sendMail(message, function(errror, info){
-            console.log("Sending Email");
             if(error){
-              console.log("Sending Email error");
               res.status(500).json({
                 error: {
                   message: error
                 }
               });
             } else {
-              console.log("Email Sent");
               res.status(200).json({
                 message: "Message Sent",
                 content: message
